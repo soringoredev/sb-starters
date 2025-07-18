3dcc091fca72a81bf561ad85a06db73e9d35dde0
Sorin Gore <soringoredev@gmail.com>
2025-07-18 18:30:06 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/sb_starters/SbStartersApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.sb_starters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStartersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/sb_starters/SbStartersApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.sb_starters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStartersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
